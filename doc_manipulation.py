05f7ea914730b2aeabf27d13cb820c3ba4062470
Vitolus <vito.davide00@gmail.com>
2024-04-18 11:14:03 +0200

    add functions

--- doc_manipulation.py
+++ doc_manipulation.py
@@ -1,5 +1,6 @@
 from faker import Faker
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 import string
 
 def generate_doc_list():
@@ -17,14 +18,25 @@ def generate_doc_list():
     doc2 = doc1.replace('cat', 'car', 1)
     return [doc1, doc2]
 
-def compute_simhash(dt_matrix, terms):
+def compute_doc_term_matrix(docs):
+    """
+    This function computes the document-term matrix
+    """
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf.fit(docs)
+    terms = np.array(tfidf.get_feature_names_out())
+    dt_matrix = np.array(tfidf.transform(docs).toarray())
+    return dt_matrix, terms
+
+def compute_simhash(dt_matrix, m):
     """
     This function computes the simhash of the documents
     """
-    # Compute the simhash of the documents
-    simhash = np.zeros((dt_matrix.shape[0], 64))
-    # TODO: chack, seems broken
-    for i, doc in enumerate(dt_matrix):
-        for j, term in enumerate(terms):
-            simhash[i] += (doc[j] > 0) * hash(term)
+    rw = np.random.choice([-1, 1], size=(dt_matrix.shape[1], m)) # random lines [term, m]
+    simhash = np.zeros((dt_matrix.shape[0], m)) # initialize signature matrix [doc, m]
+    for dt in dt_matrix:
+        for i, w in enumerate(dt):
+            simhash += w * rw[i]
+    print(simhash)
+    simhash = np.where(simhash > 0, 1, 0) # binarize signature matrix
     return simhash
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import doc_manipulation as dm
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 if __name__ == "__main__":
     docs = dm.generate_doc_list()
     print(docs)
-    tfidf = TfidfVectorizer(stop_words='english')
-    tfidf.fit(docs)
-    terms = np.array(tfidf.get_feature_names_out())
+    dt_matrix, terms = dm.compute_doc_term_matrix(docs)
     print(terms)
-    dt_matrix = np.array(tfidf.transform(docs).toarray())
     print(dt_matrix)
-    simhash = dm.compute_simhash(dt_matrix, terms)
+    m = 64
+    # simhash = dm.compute_simhash(dt_matrix, terms, m)
+    simhash = dm.compute_single_simhash()
     print(simhash)
