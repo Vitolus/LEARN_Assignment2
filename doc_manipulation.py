abe03cb11f03722a6c1f6a6d04f5fe18850a97b7
Vitolus <vito.davide00@gmail.com>
2024-04-20 23:21:11 +0200

    add computation of hamming distance

--- doc_manipulation.py
+++ doc_manipulation.py
@@ -14,7 +14,10 @@ def generate_doc_list():
     for _ in range(1):
         docs.append(fake.sentence(nb_words=10, ext_word_list=['ciao', 'cane', 'gatto'])
                     .translate(str.maketrans('', '', string.punctuation)).lower())
+        docs.append(docs[0])
     docs.append(docs[0].replace('cane', 'gatto', 1))
+    docs.append(docs[0].replace('cane', 'gatto', 3))
+    print(docs)
     return docs
 
 
@@ -26,6 +29,8 @@ def compute_doc_term_matrix(docs):
     tfidf.fit(docs)
     terms = np.array(tfidf.get_feature_names_out())
     dt_matrix = tfidf.transform(docs).toarray()
+    print(terms)
+    print(dt_matrix)
     return dt_matrix, terms
 
 
@@ -35,17 +40,20 @@ def compute_simhash(dt_matrix, m):
     """
     rw = np.random.choice([-1, 1], size=(dt_matrix.shape[1], m))  # random lines [term, m]
     simhash = np.zeros((dt_matrix.shape[0], m))  # initialize signature matrix [doc, m]
-    for i, dt in enumerate(dt_matrix):
-        for j, w in enumerate(dt):
-            simhash[i] += w * rw[j]
-    return np.where(simhash > 0, 1, 0)  # binarize signature matrix
+    for i, doc in enumerate(dt_matrix):
+        for j, term in enumerate(doc):
+            simhash[i] += term * rw[j]
+    simhash = np.where(simhash > 0, 1, 0)  # binarize signature matrix
+    return simhash
 
 
 def split_simhash(simhash, p):
     """
     This function splits the simhash in p pieces
     """
-    return np.array(np.array_split(simhash, p, axis=1))
+    simhash_pieces = np.array([np.array_split(array, p, axis=0) for array in simhash])
+    print(simhash_pieces)
+    return simhash_pieces
 
 
 def pieces_to_ints(simhash_pieces):
@@ -56,6 +64,7 @@ def pieces_to_ints(simhash_pieces):
     for i, doc in enumerate(simhash_pieces):
         for j, piece in enumerate(doc):
             simhash_ints[i][j] = int(''.join(map(str, piece)), 2)
+    print(simhash_ints)
     return simhash_ints
 
 
@@ -67,4 +76,28 @@ def ints_to_pieces(simhash_ints):
     for i, doc in enumerate(simhash_ints):
         for j, num in enumerate(doc):
             simhash_pieces[i][j] = np.array(list(int(bit) for bit in bin(int(num))[2:].zfill(32)))
+    print(simhash_pieces)
     return simhash_pieces
+
+
+def compute_hamming_distance_piece(piece1, piece2):
+    """
+    This function computes the hamming distance between the simhashes
+    """
+    hamm = bin(np.bitwise_not(np.bitwise_xor(int(piece1), int(piece2)))).count('1') - 1
+    return hamm
+
+
+def compute_hamming_distances(simhash_ints):
+    """
+    This function computes the hamming distances between the simhashes
+    """
+    hamming_distances = list()
+    for i in range(simhash_ints.shape[0] - 1):
+        pieces = list()
+        for j in range(i + 1, simhash_ints.shape[0]):
+            pieces.append(np.array([compute_hamming_distance_piece((simhash_ints[i][k]), (simhash_ints[j][k]))
+                                    for k in range(simhash_ints.shape[1])]))
+        hamming_distances.append(np.array(pieces))
+    print(hamming_distances)
+    return hamming_distances
--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import doc_manipulation as dm
+import numpy as np
 
 
 if __name__ == "__main__":
     docs = dm.generate_doc_list()
-    print(docs)
     dt_matrix, terms = dm.compute_doc_term_matrix(docs)
-    print(terms)
-    print(dt_matrix)
     m = 64
     simhash = dm.compute_simhash(dt_matrix, m)
     p = 2
     simhash_pieces = dm.split_simhash(simhash, p)
-    print(simhash_pieces)
     simhash_ints = dm.pieces_to_ints(simhash_pieces)
-    print(simhash_ints)
-    simhash_pieces = dm.ints_to_pieces(simhash_ints)
-    print(simhash_pieces)
+    hamming_distances = dm.compute_hamming_distances(simhash_ints)
