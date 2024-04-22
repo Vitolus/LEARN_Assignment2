854aae366bb92d385ce3bc95612db7e2c7ced1cf
Vitolus <vito.davide00@gmail.com>
2024-04-22 18:09:08 +0200

    trying adding spark

--- doc_manipulation.py
+++ doc_manipulation.py
@@ -14,11 +14,13 @@ def generate_synthetic_doc_list():
     Faker.seed(0)
     docs = list()
     for _ in range(1):
-        docs.append(fake.sentence(nb_words=10, ext_word_list=['ciao', 'cane', 'gatto'])
+        docs.append(fake.sentence(nb_words=15, ext_word_list=['hello', 'dog', 'cat'])
                     .translate(str.maketrans('', '', punctuation)).lower())
         docs.append(docs[0])
-    docs.append(docs[0].replace('cane', 'gatto', 1))
-    docs.append(docs[0].replace('cane', 'sasso', 3))
+        docs.append(docs[0].replace('dog', 'cat', 1))
+        docs.append(docs[0].replace('dog', 'cat', 3))
+        docs.append('the pen is on the dusted table, but the table is not clean')
+        docs.append('the pen is on the dusted floor, but the table is not clean')
     docs = pd.DataFrame(docs, columns=['text']).values.flatten()
     return docs
 
@@ -36,18 +38,24 @@ def compute_doc_term_matrix(docs):
     """
     tfidf = TfidfVectorizer(stop_words='english')
     dt_matrix = tfidf.fit_transform(docs)
-    #print('dt shape', dt_matrix.shape)
+    print('dt shape', dt_matrix.shape)
     terms = np.array(tfidf.get_feature_names_out())
-    #print('terms', terms)
-    #print('dtmatrix', dt_matrix)
+    print('terms', terms)
+    print('dtmatrix', dt_matrix)
     return dt_matrix, terms
 
+def compute_rw(n_terms, m):
+    """
+    This function computes the random lines
+    """
+    rw = np.random.choice([-1, 1], size=(n_terms, m))  # random lines [n_term, m]
+    return rw
 
-def compute_simhash(dt_matrix, n_terms, m):
+
+def compute_simhash(dt_matrix, rw):
     """
     This function computes the simhash of the documents
     """
-    rw = np.random.choice([-1, 1], size=(n_terms, m))  # random lines [n_term, m]
     simhash = dt_matrix.dot(rw)  # [n_doc, m]
     simhash = np.where(simhash > 0, 1, 0)  # binarize signature matrix
     return simhash
@@ -58,7 +66,7 @@ def split_simhash(simhash, p):
     This function splits the simhash in p pieces
     """
     simhash_pieces = np.array([np.array_split(array, p, axis=0) for array in simhash])
-    #print(simhash_pieces)
+    print(simhash_pieces)
     return simhash_pieces
 
 
@@ -67,7 +75,7 @@ def pieces_to_ints(simhash_pieces):
     This function converts each piece of simhash to integer
     """
     simhash_ints = np.apply_along_axis(lambda x: int(''.join(map(str, x)), 2), axis=2, arr=simhash_pieces)
-    #print(simhash_ints)
+    print(simhash_ints)
     return simhash_ints
 
 
@@ -89,7 +97,7 @@ def compute_hamming_distances(simhash_ints):
         pieces = [[compute_hamming_distance_piece(simhash_ints[i, k], simhash_ints[j, k])
                    for k in range(n_pieces)] for j in range(i + 1, n_docs)]
         hamming_distances.append(np.array(pieces))
-    #print(hamming_distances)
+    print(hamming_distances)
     return hamming_distances
 
 
@@ -98,5 +106,5 @@ def compute_cosine_similarities(hamming_distances, m):
     This function computes the cosine similarity between two documents
     """
     cosine_similarities = [np.cos(np.sum(doc, axis=1) / m) for doc in hamming_distances]
-    #print(cosine_similarities)
+    print(cosine_similarities)
     return cosine_similarities
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import doc_manipulation as dm
 import time
 import gc
+from pyspark.sql import SparkSession
 
-
-if __name__ == "__main__":
-    docs = dm.generate_doc_list()
-    #docs = dm.generate_synthetic_doc_list()
+def local_main():
+#docs = dm.generate_doc_list()
+    docs = dm.generate_synthetic_doc_list()
     print(docs)
     dt_matrix, terms = dm.compute_doc_term_matrix(docs)
     del docs
     gc.collect()
-    m = 128
+    m = 256
+    rw = dm.compute_rw(terms.shape[0], m)
     comp_time = time.perf_counter()
-    simhash = dm.compute_simhash(dt_matrix, terms.shape[0], m)
+    simhash = dm.compute_simhash(dt_matrix, rw)
     comp_time = time.perf_counter() - comp_time
     print('\nsimhash time', comp_time, '\n')
-    p = m/32
+    p = m/8
     simhash_pieces = dm.split_simhash(simhash, p)
     simhash_ints = dm.pieces_to_ints(simhash_pieces)
     comp_time = time.perf_counter()
@@ -26,3 +27,16 @@ if __name__ == "__main__":
     cosine_similarities = dm.compute_cosine_similarities(hamming_distances, m)
     comp_time = time.perf_counter() - comp_time
     print('\ncosine time', comp_time, '\n')
+
+
+def spark_main():
+    spark = SparkSession.builder.appName('SimHash').getOrCreate()
+    sc = spark.sparkContext
+    df = spark.createDataFrame(dm.generate_synthetic_doc_list())
+    df.show()
+    df.printSchema()
+
+
+if __name__ == "__main__":
+    #local_main()
+    spark_main()
