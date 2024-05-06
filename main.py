15402ebf0c6c807057f25066d73c1f87644933c1
Vitolus <vito.davide00@gmail.com>
2024-05-06 12:18:49 +0200

    test on laptop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
-import os
+#from dotenv import load_dotenv
+#import os
 import sys
 import doc_manipulation as dm
 import time
@@ -17,7 +17,6 @@ def mapper(spark, docs, m, p):
     the next phase.
     """
     comp_time = time.perf_counter()
-    # docs, n_terms = dm.compute_tfidf(docs.toDF(['index', 'text']))
     docs, n_terms = dm.compute_tfidf(docs)
     print('\ntfidf time', time.perf_counter() - comp_time, '\n')
     print(docs.take(10))
@@ -68,29 +67,35 @@ def reducer(mapped, simhash_pieces, m, s):
             for j in range(i + 1, len(values)):  # for each other document
                 doc_id2, doc2 = values[j]  # get the document id and the document
                 doc2 = np.array(doc2)  # convert the document to a numpy array
-                # if the key is not the maximum of the intersection of doc1 and doc2
+                # if the key is not the maximum of the intersection of doc1 and doc2 skip this iteration, this
+                # guarantees that two documents are only compared once
                 if key != max(np.intersect1d(doc1, doc2)):
-                    continue  # skip this iteration
+                    continue
                 # count the number of shared pieces between doc1 and doc2
                 shared_pieces = dm.count_shared_pieces(doc1, doc2)
                 # if the number of shared pieces is greater than or equal to half of the length of doc1
                 if shared_pieces >= len(doc1) // 2:
                     # compute the hamming distance between doc1 and doc2
                     hamming = dm.compute_hamming_distance(doc1, doc2)
-                    # compute the cosine similarity between doc1 and doc2
-                    similarity = dm.compute_cosine_similarity(hamming, m)
-                    if similarity >= s:  # if the cosine similarity is greater than or equal to the threshold s
+                    # if the hamming distance is less than or equal to s bits
+                    if hamming <= s:
+                        # compute the cosine similarity between doc1 and doc2
+                        similarity = dm.compute_cosine_similarity(hamming, m)
                         yield (doc_id1, doc_id2), similarity
 
     return grouped.flatMap(lambda x: reduce_func(x[0], x[1]))  # return the reduced RDD
 
 
-def spark_main(m=64, p=8, s=0.95):
+def spark_main(path="/home/claudio.lucchese/datasets/enron/*", m=64, p=8, s=3):
     spark = SparkSession.builder.appName('SimHash').getOrCreate()  # create a Spark session
-    # docs = dm.generate_synthetic_doc_list(spark)  # generate a list of synthetic documents
-    # print(docs.take(10))
-    docs = dm.generate_doc_list(spark)  # generate a list of documents
-    docs.show(10)
+    # document generation for testing
+    docs = dm.generate_synthetic_doc_list(spark).toDF(['index', 'text'])
+    print(docs.take(10))
+
+    # document generation on real data
+    #docs = dm.generate_doc_list(spark, path)  # generate a list of documents
+    #docs.show(10)
+
     comp_time = time.perf_counter()
     mapped, simhash_pieces = mapper(spark, docs, m, p)  # map phase
     comp_time = time.perf_counter() - comp_time
@@ -106,18 +111,22 @@ def spark_main(m=64, p=8, s=0.95):
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    os.environ["PYSPARK_PYTHON"] = os.getenv("PYSPARK_PYTHON")
-    pyspark_python = os.environ.get("PYSPARK_PYTHON", None)
-    if pyspark_python:
-        print(f"PySpark is using this Python interpreter: {pyspark_python}")
-    else:
-        print("PySpark is using the system's default Python interpreter.")
+    #load_dotenv()
+    #os.environ["PYSPARK_PYTHON"] = os.getenv("PYSPARK_PYTHON")
+    #pyspark_python = os.environ.get("PYSPARK_PYTHON", None)
+    #if pyspark_python:
+    #    print(f"PySpark is using this Python interpreter: {pyspark_python}")
+    #else:
+    #    print("PySpark is using the system's default Python interpreter.")
+
     if len(sys.argv) == 1:  # if no arguments are provided
         spark_main()  # call the main function with default arguments
-    elif len(sys.argv) == 4:  # if three arguments are provided
-        # call the main function with the provided arguments
-        spark_main(int(sys.argv[1]), int(sys.argv[2]), float(sys.argv[3]))
+    elif len(sys.argv) == 2:  # if 1 argument is provided
+        # call the main function with onlt the path argument
+        spark_main(sys.argv[1])
+    elif len(sys.argv) == 5:  # if 4 arguments are provided
+        # call the main function with all the arguments
+        spark_main(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), float(sys.argv[4]))
     else:  # if an invalid number of arguments is provided
-        print("Usage: spark-submit main.py <m> <p> <s>")
+        print("Usage: spark-submit main.py <path> [<m> <p> <s>]")
         sys.exit(1)
