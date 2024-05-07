3e24f84410d8cca009588fde88f4f2a28170cfa9
Vitolus <vito.davide00@gmail.com>
2024-05-07 17:42:02 +0200

    optimizations

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ def mapper(spark, docs, m, p):
     comp_time = time.perf_counter()
     docs, n_terms = dm.compute_tfidf(docs)
     print('\ntfidf time', time.perf_counter() - comp_time, '\n')
-    print(docs.take(10))
     comp_time = time.perf_counter()
     rw = dm.compute_rw(spark, n_terms, m)
     print('\nrw time', time.perf_counter() - comp_time, '\n')
@@ -33,11 +32,11 @@ def mapper(spark, docs, m, p):
 
     def map_func(doc):
         doc_id, pieces = doc  # doc is a tuple (doc_id, pieces)
-        pieces = np.sort(pieces)[::-1]  # sort the pieces in descending order
+        pieces = np.sort(pieces)  # sort the pieces in ascending order
         # take the first half of the pieces to reduce the number of copies across nodes,
         # at the cost of some accuracy in the number of similar pairs
         pieces = pieces[:len(pieces)//2]
-        for piece in pieces:  # for each piece in the first half of the pieces
+        for piece in pieces:
             yield piece, doc_id
 
     return simhash_pieces.flatMap(map_func), simhash_pieces  # return the mapped RDD and the simhash pieces
@@ -46,8 +45,7 @@ def mapper(spark, docs, m, p):
 def reducer(mapped, simhash_pieces, m, s):
     """
     Reduce Phase:
-    - Group by Piece: Group the documents that have at least half of the pieces of the SimHash equal. This is done by
-    Spark under the hood when you call groupByKey() or similar operations.
+    - Group by Piece: Group the documents that have at least half of the pieces of the SimHash equal.
     - Compute Hamming Distance: For each group of documents (that share at least half of the SimHash pieces), compute
     the Hamming distance between the SimHashes of each pair of documents.
     - Compute Cosine Similarity: Still within each group, compute the cosine similarity for each pair of documents.
@@ -61,15 +59,15 @@ def reducer(mapped, simhash_pieces, m, s):
 
     def reduce_func(key, values):
         values = sorted(values, key=lambda x: x[0])  # sort the values by doc_id
-        for i in range(len(values)):  # for each document
-            doc_id1, doc1 = values[i]  # get the document id and the document
-            doc1 = np.array(doc1)  # convert the document to a numpy array
-            for j in range(i + 1, len(values)):  # for each other document
-                doc_id2, doc2 = values[j]  # get the document id and the document
-                doc2 = np.array(doc2)  # convert the document to a numpy array
-                # if the key is not the maximum of the intersection of doc1 and doc2 skip this iteration, this
+        for i in range(len(values)):
+            doc_id1, doc1 = values[i]
+            doc1 = np.array(doc1)
+            for j in range(i + 1, len(values)):
+                doc_id2, doc2 = values[j]
+                doc2 = np.array(doc2)
+                # if the key is not the minimum of the intersection of doc1 and doc2 skip this iteration, this
                 # guarantees that two documents are only compared once
-                if key != max(np.intersect1d(doc1, doc2)):
+                if key != min(np.intersect1d(doc1, doc2)):
                     continue
                 # count the number of shared pieces between doc1 and doc2
                 shared_pieces = dm.count_shared_pieces(doc1, doc2)
@@ -78,32 +76,38 @@ def reducer(mapped, simhash_pieces, m, s):
                     # compute the hamming distance between doc1 and doc2
                     hamming = dm.compute_hamming_distance(doc1, doc2)
                     # if the hamming distance is less than or equal to s bits
-                    if hamming <= s:
+                    if hamming <= m * s:
                         # compute the cosine similarity between doc1 and doc2
                         similarity = dm.compute_cosine_similarity(hamming, m)
-                        yield (doc_id1, doc_id2), similarity
+                        # exclude the pair of documents that are euqal
+                        if similarity < 1.0:
+                            yield (doc_id1, doc_id2), similarity
 
     return grouped.flatMap(lambda x: reduce_func(x[0], x[1]))  # return the reduced RDD
 
 
-def spark_main(path="/home/claudio.lucchese/datasets/enron/*", m=64, p=8, s=3):
+def spark_main(path="/home/claudio.lucchese/datasets/enron/*", m=64, p=8, s=1.0):
     spark = SparkSession.builder.appName('SimHash').getOrCreate()  # create a Spark session
     # document generation for testing
-    docs = dm.generate_synthetic_doc_list(spark).toDF(['index', 'text'])
-    print(docs.take(10))
+    #docs = dm.generate_synthetic_doc_list(spark).toDF(['index', 'text'])
+    #docs.persist()
+    #print(docs.take(10))
 
     # document generation on real data
-    #docs = dm.generate_doc_list(spark, path)  # generate a list of documents
-    #docs.show(10)
+    docs = dm.generate_doc_list(spark, path).limit(10000)  # generate a list of documents
+    #docs.persist()
+    docs.show(10)
 
     comp_time = time.perf_counter()
     mapped, simhash_pieces = mapper(spark, docs, m, p)  # map phase
+    #docs.unpersist()
     comp_time = time.perf_counter() - comp_time
     print('\nmap phase time', comp_time, '\n')
     print(mapped.take(10))
     comp_time = time.perf_counter()
     reduced = reducer(mapped, simhash_pieces, m, s)  # reduce phase
     comp_time = time.perf_counter() - comp_time
+    #reduced.persist()
     print('\nreduce phase time', comp_time, '\n')
     print(reduced.take(10))
     print('\nnumber of similar pairs', reduced.count())
