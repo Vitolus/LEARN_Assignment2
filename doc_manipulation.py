2c222e4cee88e562c02f06d9fe43e2f1e9c9b0d5
Vitolus <vito.davide00@gmail.com>
2024-05-08 21:41:27 +0200

    add check on file extension

--- doc_manipulation.py
+++ doc_manipulation.py
@@ -4,14 +4,20 @@ from pyspark.sql import functions as F
 from pyspark.sql import Window
 from pyspark.ml.linalg import Vectors
 import math
+import sys
 
 
-def generate_doc_list(spark, type, path):
-    if type == 'parquet':
+def generate_doc_list(spark, ext, path):
+    docs = None
+    if ext == 'parquet':
         docs = spark.read.parquet(path).withColumn('index', F.lit(0))
-    elif type == 'csv':
+    elif ext == 'csv':
         docs = spark.read.csv(path, header=True).withColumn('index', F.lit(0))
         docs = docs.select("summary").rdd.flatMap(lambda x: x).withColumn('index', F.lit(0))
+    else:
+        print('Invalid extension')
+        spark.stop()
+        sys.exit(1)
     return docs.withColumn("index", F.row_number().over(Window.partitionBy('index').orderBy(F.lit(0))) - 1)
 
 
